c4d4f35dde981709442a39518bdf30cfa93b7e65
Vinod Gullipalli <vinod.g@doodleblue.com>
2023-04-27 23:45:22 +0530

    new pushes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments").then((response) =>
+      response.json().then((json) => setData(json))
+    );
+  }, []);
+  const PreviousPerson = () => {
+    setNumber(number - 1);
+  };
+  const UpdatePerson = () => {
+    console.log(":::::", UpdatePerson);
+    setNumber(number + 1);
+  };
+  // const [data, setData] = useState([]);
+  // // const [joke, setJoke] = useState("");
+  // useEffect(() => {
+  //   fetch("https://reqres.in/api/products/3")
+  //     .then((data) => data.json())
+  //     .then((result) => setData(result.support));
+  // });
+  // const getJoke = (data) => {
+  //   console.log("::::>", data);
+  //   fetch("https://official-joke-api.appspot.com/jokes/programming/random")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setJoke(data[0].setup + data[0].punchline);
+  //     });
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* api Call<button onClick={getJoke}>getJokeRightNow</button>
+      {joke} */}
+      {/* {/* <h1>Get Api</h1> */}
+      <h2>
+        <span>Name:</span>
+        {data.length && data[number].postId}
+      </h2>
+      <button onClick={PreviousPerson}>Previous</button>
+      <button disabled={number >= 499} onClick={UpdatePerson}>
+        Next
+      </button>
+      {/* <h2>{data.text}</h2>
+        <h1>{data.url}</h1> */}
     </div>
   );
 }
